5aef86f669a5478802d170640c0fbe91a4338804
Samekuelisiwed <ps_dlamini@ymail.com>
2023-05-04 10:07:38 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_numbers(void);
+int mul(int a, int b);
+int _isdigit(int c);
+int _isupper(int c);
+void print_most_numbers(void);
