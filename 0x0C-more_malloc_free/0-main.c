e9f4ca60c0411da4f2b50a0e695d336aac9748ca
Samekuelisiwed <ps_dlamini@ymail.com>
2023-05-21 01:22:50 -0700

    Trust no one

--- 0x0C-more_malloc_free/0-main.c
+++ 0x0C-more_malloc_free/0-main.c
@@ -4,34 +4,27 @@
 #include <limits.h>
 
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 
 {
-
 char *c;
 int *i;
 float *f;
 double *d;
 
-c = (char *)malloc(sizeof(char) * 1024);  /* Use malloc instead of malloc_checked*/
+c = malloc_checked(sizeof(char) * 1024);
 printf("%p\n", (void *)c);
-i = (int *)malloc(sizeof(int) * 402);
+i = malloc_checked(sizeof(int) * 402);
 printf("%p\n", (void *)i);
-f = (float *)malloc(sizeof(float) * 100000000);
+f = malloc_checked(sizeof(float) * 100000000);
 printf("%p\n", (void *)f);
-d = (double *)malloc(INT_MAX);
+d = malloc_checked(INT_MAX);
 printf("%p\n", (void *)d);
-
-if (c == NULL || i == NULL || f == NULL || d == NULL) {
-/* Check if memory allocation was successful*/
-printf("Memory allocation failed\n");
-return 1;
-}
 free(c);
 free(i);
 free(f);
